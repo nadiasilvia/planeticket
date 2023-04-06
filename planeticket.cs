1b272ec9697d695de3a34acfb80bf1a4d264b44c
Nadia Silvia <128023879+nadiasilvia@users.noreply.github.com>
2023-04-06 14:41:32 +0700

    Add files via upload

---
+++ planeticket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planeticket
+{
+    public class planeticket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine ("Origin: {0}", Origin);
+            Console.WriteLine ("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0} ({1})", Cost, Currency);
+            if (Currency == "IDR")
+            {
+                Cost = Cost / 15000;
+                Console.WriteLine("Convert to Dollar: {0}", Cost);
+            } else if (Currency == "USD"){
+                Cost = Cost * 15000;
+                Console.WriteLine("Convert to Rupiah: {0}", Cost);
+            }
+
+        }
+    }
+}
